551f17c21c04d4da4e13dae37bca142357ee8fdd
david bassem <davidbassem694@@gmail.com>
2023-08-21 10:31:30 -0700

    a

--- 0x05-pointers_arrays_strings/6-puts2.c
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -1,29 +1,31 @@
-i#include "main.h"
+#include "main.h"
+
 /**
- * puts2 - function should print only one character out of two
- * starting with the first one
- * @str: input
- * Return: print
- */
+
+puts2 - function should print only one character out of two
+
+starting with the first one
+
+@str: input
+
+Return: none
+*/
 void puts2(char *str)
 {
-	int longi = 0;
-	int t = 0;
-	char *y = str;
-	int o;
+int length = 0;
+int i;
 
-	while (*y != '\0')
-	{
-		y++;
-		longi++;
-	}
-	t = longi - 1;
-	for (o = 0 ; o <= t ; o++)
-	{
-		if (o % 2 == 0)
-	{
-		_putchar(str[o]);
-	}
-	}
-	_putchar('\n');
+/* Calculate the length of the string */
+while (str[length] != '\0')
+{
+length++;
+}
+
+/* Print every other character */
+for (i = 0; i < length; i += 2)
+{
+_putchar(str[i]);
+}
+
+_putchar('\n');
 }
